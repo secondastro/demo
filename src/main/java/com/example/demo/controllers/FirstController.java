18f4c1bc829f57e4ee60e6695204518078329319
User <anton_trenogin@mail.ru>
2023-01-24 21:10:09 +0500

    demo budget app

---
+++ src/main/java/com/example/demo/controllers/FirstController.java
@@ -0,0 +1,2 @@
+package com.example.demo.Controllers;public class FirstController {
+}
---
+++ src/main/java/com/example/demo/services/impl/BudgetServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.demo.services;
+
+public class BudgetServiceImpl implements BudgetService{
+    @Override
+    public int getDailyBudget() {
+        return 0;
+    }
+
+    @Override
+    public int getBalance() {
+        return 0;
+    }
+}
